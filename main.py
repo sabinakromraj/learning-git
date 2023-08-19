a8259889aaac74755baad895a9d2a4ee25eb0295
Sabina Kromraj <sabina.kromraj@gmail.com>
2023-08-19 23:42:45 +0200

    add capitalize main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ shopping_dict = {
     'piekarnia': ['chleb', 'pączek', 'bułki'],
     'warzywniak': ['marchew', 'seler', 'rukola']
 }
+capi_shopping_dict = {key.capitalize(): [value.capitalize() for value in values] for key, values in shopping_dict.items()}
 
 value_count = 0
 shop_count = 0
 
-for key, value in shopping_dict.items():
+for key, value in capi_shopping_dict.items():
     print(f"Idę do {key}, kupuję tu następujące rzeczy: {value}")
     value_count += len(value)
     shop_count += 1
